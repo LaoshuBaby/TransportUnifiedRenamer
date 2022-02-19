2cadd7f77ac261282e60087bbe7bc0e69454a6fc
LaoshuBaby <keaitianxinmail@qq.com>
2022-02-19 15:33:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-preset_pattern=[
-    "",
-    "",
-]
+preset_pattern={
+    "1":"<ref><classifier><colon><from><arrow><to>"
+}
+
+def proposal_1(schema:int):
+    if schema==1:
+        return pass
+
+def proposal_2(schema:int):
+    if schema==1:
+        return pass
 
 def is_chinese_ref(ref:str):
-    if ref.find("路")!=-1 or ref.find("号")!=-1:
-        return True
-    else:
-        return False
+    pass
 
 def classifier(type:str,ref:str):
     if type=="bus":
         return "路"
     elif type=="subway":
-        if ref.
+        if is_chinese_ref(ref)==True:
+            return "线"
+        else:
+            return "号线"
+    else:
+        return ""
 
 def renamer(from:str, to:str, ref:str, type:str, network:str):
 
