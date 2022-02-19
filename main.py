34edccb0544629fde8c687a83f3174a2f46a077d
LaoshuBaby <keaitianxinmail@qq.com>
2022-02-19 15:28:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ preset_pattern=[
     "",
 ]
 
+def is_chinese_ref(ref:str):
+    if ref.find("路")!=-1 or ref.find("号")!=-1:
+        return True
+    else:
+        return False
+
+def classifier(type:str,ref:str):
+    if type=="bus":
+        return "路"
+    elif type=="subway":
+        if ref.
+
 def renamer(from:str, to:str, ref:str, type:str, network:str):
 
 
